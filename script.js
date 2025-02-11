2a1e73600a03414f094126b7a8856f21106046c9
Francisco Mazzi <franciscomazzi110@gmail.com>
2025-02-11 20:27:23 -0300

    act 1

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    document.getElementById("nav-links").classList.toggle("active");
+});
+let currentIndex = 0;
+const sections = document.querySelectorAll("section");
+
+function scrollToSection(index) {
+    if (index >= 0 && index < sections.length) {
+        sections[index].scrollIntoView({ behavior: "smooth" });
+        currentIndex = index;
+    }
+}
+
+function scrollUp() {
+    scrollToSection(currentIndex - 1);
+}
+
+function scrollDown() {
+    scrollToSection(currentIndex + 1);
+}
+
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowDown") {
+        scrollDown();
+    } else if (event.key === "ArrowUp") {
+        scrollUp();
+    }
+});
